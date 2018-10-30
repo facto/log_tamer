9b4ae52980930eb127f0386c1fc09b61940670c7
Joshua Rieken <joshua@joshuarieken.com>
2018-10-30 11:43:59 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule LogTamer.MixProject do
       version: "0.5.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
+      description: description(),
       deps: deps()
     ]
+  end
+
+  def description do
+    "Capture, flush and resume logging in the Elixir mix console."
   end
 
   # Run "mix help compile.app" to learn about applications.
